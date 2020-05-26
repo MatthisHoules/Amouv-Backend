962e6a0aeffada6f56d16d4690ea17882c51e0d9
Matthis HOULES <48548675+dyltha@users.noreply.github.com>
2020-05-26 14:41:57 +0200

    Adding design User part

--- Public/Views/Sign/SignIn.php
+++ Public/Views/Sign/SignIn.php
@@ -12,21 +12,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Amouv | connexion</title>
 
     <link rel="stylesheet" href="./Public/assets/css/popup.css">
     <script src="./Public/assets/js/popup.js" defer></script>
+
+    <link rel="stylesheet" href="./Public/assets/css/sign.css">
+    <script src="./Public/assets/js/sign.js" defer></script>
 
     <script src="https://kit.fontawesome.com/b18ab37082.js" crossorigin="anonymous"></script>
 </head>
 <body>
     
-    <form action="" method="post">
+<img src="./Public/assets/images/amouvLogoBlack.svg" alt="" class="signLogo">
+    <main>
+        <a href="" class="backButton">
+            <i class="fas fa-chevron-left"></i>
+        </a>
         
-        <input type="email" name="mailInput" id="">
-        <input type="password" name="pwdInput" id="">
-        <input type="submit" value="connexion" name="submit">
-    </form>
+        <form action="" method="POST">
+            <p class="signTitle">
+                Connectez-vous
+            </p>
+
+            <div class="inputContainer">
+                <label for="mailInput">Adresse E-mail</label>
+                <input type="email" name="mailInput" id="mailInput">
+            </div>
+
+            <div class="inputContainer">
+                <label for="pwdInput">Mot de passe</label>
+                <div class="pwdInput">
+                    <input type="password" name="pwdInput" id="pwdInput">
+                    <button class="pwdTrigger" data-toggle="pwdInput" type="button">
+                        <i class="far fa-eye"></i>
+                    </button>
+                </div>
+            </div>
+
+            <div class="buttonContainer">
+                <button type="submit" name="submit" value="submit">
+                    Connexion
+                </button>
+            </div>
+
+        </form>
+    </main>
+
 
 </body>
 </html>
--- Public/Views/Sign/SignUp.php
+++ Public/Views/Sign/SignUp.php
@@ -12,29 +12,73 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Amouv | inscription</title>
 
     <link rel="stylesheet" href="./Public/assets/css/popup.css">
     <script src="./Public/assets/js/popup.js" defer></script>
+
+    <link rel="stylesheet" href="./Public/assets/css/sign.css">
+    <script src="./Public/assets/js/sign.js" defer></script>
 
     <script src="https://kit.fontawesome.com/b18ab37082.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
-    <form action="" method="post">
-        <input type="email" name="mailInput" id="" placeholder="Email">
+<img src="./Public/assets/images/amouvLogoBlack.svg" alt="" class="signLogo">
+    <main>
+        <a href="" class="backButton">
+            <i class="fas fa-chevron-left"></i>
+        </a>
+        
+        <form action="" method="POST">
+            <p class="signTitle">
+                Inscrivez-vous
+            </p>
 
-        <input type="text" name="firstnameInput" id="" placeholder="prenom">
-        <input type="text" name="lastnameInput" id="" placeholder="nom">
+            <div class="inputContainer">
+                <label for="mailInput">Adresse E-mail</label>
+                <input type="email" name="mailInput" id="mailInput">
+            </div>
 
+            <div class="inputContainer">
+                <label for="firstnameInput">Prénom</label>
+                <input type="text" name="firstnameInput" id="firstnameInput">
+            </div>
 
-        <input type="password" name="passwordInput" id="" placeholder="pwd">
-        <input type="password" name="RpasswordInput" id="" placeholder="répeter pwd">
+            <div class="inputContainer">
+                <label for="lastnameInput">Nom de famille</label>
+                <input type="text" name="lastnameInput" id="lastnameInput">
+            </div>
 
-        <button type="submit" name="submit" value="submit">
-            Inscription
-        </button>
-    </form>
+            <div class="inputContainer">
+                <label for="passwordInput">Mot de passe</label>
+                <div class="pwdInput">
+                    <input type="password" name="passwordInput" id="passwordInput">
+                    <button class="pwdTrigger" data-toggle="passwordInput" type="button">
+                        <i class="far fa-eye"></i>
+                    </button>
+                </div>
+            </div>
+
+            <div class="inputContainer">
+                <label for="RpasswordInput">Répeter Mot de passe</label>
+                <div class="pwdInput">
+                    <input type="password" name="RpasswordInput" id="RpasswordInput">
+                    <button class="pwdTrigger" data-toggle="RpasswordInput" type="button">
+                        <i class="far fa-eye"></i>
+                    </button>
+                </div>
+            </div>
+
+            <div class="buttonContainer">
+                <button type="submit" name="submit" value="submit">
+                    Inscription
+                </button>
+            </div>
+
+        </form>
+    </main>
+
     
 </body>
 </html>
