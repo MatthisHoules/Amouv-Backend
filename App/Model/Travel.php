855f2d2e4b858711fa0b10ca486bdaaab1104a3a
Matthis HOULES <48548675+dyltha@users.noreply.github.com>
2020-06-01 15:32:01 +0200

    DB interaction for travel search, resuts and focus

--- App/Model/Travel.php
+++ App/Model/Travel.php
@@ -110,14 +110,14 @@ class Travel extends Model {
      *  @brief : get a travel if it exist
      * 
      */
-    public function getTravel($travelId) {
+    public static function getTravel($travelId) {
 
         $DB = static::DBConnect();
 
         $stmt = $DB->prepare('SELECT * 
                               FROM `travel`
-                              JOIN `car` ON `travel.id_car` = `car.id`
-                              JOIN `user` ON `user.id` = `travel.id_Creator`
+                              JOIN `car` ON `travel`.`id_car` = `car`.`car_id`
+                              JOIN `user` ON `user`.`id` = `travel`.`id_Creator`
                               WHERE `id` = ?
                             ');
         $stmt->execute([$travelId]);
@@ -129,15 +129,106 @@ class Travel extends Model {
         }
 
         return new Travel (
+            $result[0]['travel_id'],
+            new User(
+                        $result[0]['email'],
+                        $result[0]['lastname'],
+                        $result[0]['firstname'],
+                        $result[0]['profileimg'],
+                        1,
+                        $result[0]['Id']
+                    ),
+            null,
+            $result[0]['departure'],
+            $result[0]['arrival'],
+            $result[0]['seats'],
+            $result[0]['smoking'],
+            $result[0]['lugage']
             
         );
 
-        
-
-
+    
     }
     
 
 
+    /**
+     *  @name : searchTravels
+     * 
+     *  @param $cityStart start city
+     *  @param $cityEnd end city
+     *  @param $dateD date departure
+     * 
+     *  @return array
+     */
+    public static function searchTravels($cityStart, $cityEnd, $dateD) {
+
+
+        $DB = static::DBConnect();
+
+        $stmt = $DB->prepare("SELECT *
+                              FROM `Travel`
+                              JOIN `user` ON `id_creator` = `user`.`Id`
+                              WHERE `departure` = ?
+                              AND `arrival` = ?
+                              AND 
+                                (
+                                    `date_dep` >= DATE_SUB(FROM_UNIXTIME(?), INTERVAL 12 HOUR)
+                                    OR `date_dep` <= DATE_SUB(FROM_UNIXTIME(?), INTERVAL -12 HOUR)
+                                )
+                              ORDER BY ABS(date_dep - FROM_UNIXTIME(?))
+                            ");
+        $stmt->execute([$cityStart, $cityEnd, $dateD, $dateD, $dateD]);
+
+        $result = $stmt->fetchAll();
+
+        $listTravel = [];
+        for($i = 0; $i < sizeof($result); ++$i) {
+            array_push($listTravel, new Travel(
+                $result[$i]['travel_id'],
+                new User(
+                            $result[$i]['email'],
+                            $result[$i]['lastname'],
+                            $result[$i]['firstname'],
+                            $result[$i]['profileimg'],
+                            1,
+                            $result[$i]['Id']
+                        ),
+                null,
+                $result[$i]['departure'],
+                $result[$i]['arrival'],
+                $result[$i]['seats'],
+                $result[$i]['smoking'],
+                $result[$i]['lugage']
+            ));
+        }
+
+        return $listTravel;
+
+    } // public static function searchTravels($cityStart, $cityEnd, $dateD)
+
+
+
+
+
+    public function getId(){
+		return $this->id;
+	}
+
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getUser(){
+		return $this->user;
+	}
+
+	public function setUser($user){
+		$this->user = $user;
+	}
+
+	public function getCar(){
+		return $this->car;
+	}
 
 }
